fa81a81e90b4623bbaf7e9a2ee2d46e4f26556e9
Maurice Ibrahim <rockygo2@outlook.com>
2024-06-14 12:41:18 +0200

    added escape_markdown and small fix to _test_string

--- discord/utils.py
+++ discord/utils.py
@@ -832,7 +832,7 @@ def _string_width(string: str) -> int:
     UNICODE_WIDE_CHAR_TYPE = 'WFA'
     func = unicodedata.east_asian_width
     return sum(
-        2 if (mark(2, "_string_width"), func(char))[1] in UNICODE_WIDE_CHAR_TYPE else (1, mark(2, "_string_width"))[0]
+        2 if (mark(2, "_string_width"), func(char))[1] in UNICODE_WIDE_CHAR_TYPE else (1, mark(3, "_string_width"))[0]
         for char in string
     )
 
@@ -949,6 +949,7 @@ def remove_markdown(text: str, *, ignore_links: bool = True) -> str:
     return re.sub(regex, replacement, text, 0, re.MULTILINE)
 
 
+@test(5)
 def escape_markdown(text: str, *, as_needed: bool = False, ignore_links: bool = True) -> str:
     r"""A helper function that escapes Discord's markdown.
 
@@ -980,15 +981,20 @@ def escape_markdown(text: str, *, as_needed: bool = False, ignore_links: bool = 
             groupdict = match.groupdict()
             is_url = groupdict.get('url')
             if is_url:
+                mark(0, "escape_markdown")
                 return is_url
+            mark(1, "escape_markdown")
             return '\\' + groupdict['markdown']
 
         regex = _MARKDOWN_STOCK_REGEX
         if ignore_links:
             regex = f'(?:{_URL_REGEX}|{regex})'
+            mark(4)
+        mark(2)
         return re.sub(regex, replacement, text, 0, re.MULTILINE)
     else:
         text = re.sub(r'\\', r'\\\\', text)
+        mark(3)
         return _MARKDOWN_ESCAPE_REGEX.sub(r'\\\1', text)
 
 
