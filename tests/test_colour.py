d5de3d9bd413d9ddab2facc30d57f3016284db57
FrankLawrence <frankl.am.htg@icloud.com>
2024-06-17 18:48:05 +0200

    Add test

--- tests/test_colour.py
+++ tests/test_colour.py
@@ -62,6 +62,8 @@ def test_from_str(value, expected):
         'rgb(30, -1, 60)',
         'invalid(a, b, c)',
         'rgb(',
+        '',
+        '0x#',
     ],
 )
 def test_from_str_failures(value):
